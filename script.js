6f8d5a86361572013cebaa247819f29db76d4e15
kozyrev42 <kozyrev054@gmail.com>
2022-08-01 11:28:42 +0700

    подключил библиотеку, обратился к тегу

---
+++ script.js
@@ -0,0 +1,13 @@
+//  Работа с библиотекой jQuery осуществляется с помощью универсальной функции $() 
+
+// let str = "   str   ";
+// alert(str);
+// alert($.trim(str));
+
+// let elems = $('#www').html ();
+// alert(elems);
+// console.log(elems);
+
+
+alert($('#test').html());
+console.log($('#test').html());
